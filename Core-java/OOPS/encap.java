9c89055cb7e529b738279a25b054e31c6203a72a
Avni Sharma <sharma.avni0102@gmail.com>
2026-06-21 17:18:28 -1100

    encapsulation eg

---
+++ Core-java/OOPS/encap.java
@@ -0,0 +1,33 @@
+class Student {
+
+    private int age;
+
+
+    public void setAge(int age){
+
+        this.age = age;
+
+    }
+
+
+    public int getAge(){
+
+        return age;
+
+    }
+
+}
+
+
+class encap {
+
+    public static void main(String[] args){
+
+        Student s1 = new Student();
+
+        s1.setAge(21);
+
+        System.out.println(s1.getAge());
+
+    }
+}
